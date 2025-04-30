495a569897a23a2e790f9ef4457248658ebc4a0d
Israel Quiroz <israquirozz@MacBook-Pro-de-Israel.local>
2025-04-30 19:17:22 +0200

    Fin index.php (se incluye navbar y footer)

---
+++ components/navbar.php
@@ -0,0 +1,7 @@
+<nav class="navbar flex-c">
+  <a href="#" class="logo"><h2>LOGO</h2></a>
+  <div class="buttons">
+    <a href="#"><button class="btn">Registrarse</button></a>
+    <a href="#"><button class="btn">Iniciar Sesión</button></a>
+  </div>
+</nav>
